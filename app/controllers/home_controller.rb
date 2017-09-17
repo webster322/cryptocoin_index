598e9f5956468b0e6f7fb217674f0c98c03d7486
Mateusz Kaczmarek <mk16939@gmail.com>
2017-09-17 13:41:23 +0000

    Added pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
+  require 'will_paginate/array'
   def index
-    response = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/?limit=50')
-    @json = JSON.parse(response.body)
+    response = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
+    data = JSON.parse(response.body)
+    @json = data.paginate(page: params[:page], per_page: 10)
   end
 end
