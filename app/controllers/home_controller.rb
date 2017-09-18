8fa57d1029d7281753899a4c1973b962d753416c
Mateusz Kaczmarek <mk16939@gmail.com>
2017-09-18 21:05:55 +0000

    Added logo and choice of numbers row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   require 'will_paginate/array'
   def index
+    if params[:num].nil?
+      params[:num] = 10
+    end
     response = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/')
     data = JSON.parse(response.body)
-    @json = data.paginate(page: params[:page], per_page: 10)
+    @json = data.paginate(page: params[:page], per_page: params[:num])
   end
 end
